1f874a5061841d7b0d68e6ae539acac5696aade1
Anas Yousif <ayousif0313@mymail.stratford.edu>
2020-01-02 22:27:00 -0500

    I added new property

--- Source/MyApp.Web/MyApp.Web/Models/ErrorViewModel.cs
+++ Source/MyApp.Web/MyApp.Web/Models/ErrorViewModel.cs
@@ -4,6 +4,7 @@ namespace MyApp.Web.Models
 {
     public class ErrorViewModel
     {
+        public int MyProperty { get; set; }
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
